843879fbb053420978f6b147b963ca16524587ac
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-01-31 19:28:49 +0000

    BMI

---
+++ week02/BMI.py
@@ -0,0 +1,7 @@
+# BMI 
+# Author: Ricardo Rodrigues
+
+# we must convert to number be real number to add a real number
+number =  int(input('please enter your number:'))
+newNumber = number + 1
+print ('{} plus one is {}' .format(number, newNumber))
