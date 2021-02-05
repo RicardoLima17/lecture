53b4b805bbe9c3ceb1db1438dbbd91497ddc604c
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-05 22:00:26 +0000

    'lab2.1'

---
+++ week02/lab2.1.py
@@ -0,0 +1,37 @@
+# Use the console to see what 2 + 4
+print(2+4)
+print(2 + 4)
+
+# a. subtract 5 from 8
+print(5-8)
+
+# Multiple 21 by 55
+print (21 * 55)
+
+# Divide 21 by 4 and get a decimal result
+print (21/4)
+
+# Divide 21 by 4 and get a interger result
+print (int (21/4))
+
+# Get the remainder of 21 divided by 4
+print (21%4)
+
+# work out 2 to power of 8
+print (2**8)
+
+# is 2 equal to 3
+print ( 2==3)
+
+# is 2 not equal to 3
+print ( 2!=3)
+
+# Is 3 greater than 3
+print (3>3)
+
+# Is 3 greater or equal to 3
+print(3>=3)
+
+print ( 3-3-3-3+
+20
+)
