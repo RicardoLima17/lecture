12d25bb8933884263e9545cfa9a6c8386072d02e
RicardoLima17 <77755223+RicardoLima17@users.noreply.github.com>
2021-02-07 14:59:59 +0000

    Update BMI.py

--- week02/BMI.py
+++ week02/BMI.py
@@ -8,8 +8,19 @@ height =   float(input('please enter your Height in centimeter: '))
 # BMI is the variable of what did you type for weight and height 
 BMI =(weight / (height ** 2)) * 10000
 
-# print BMI and I used a function round 2 decimal 
-print ('Your BMI: ', (round(BMI,2)))
+# Conditions if: the result is less than 18.5 
+if BMI < 18.5:
+        print("A person with a BMI of " + str((round(BMI,2))) + " is underwieght ")
+
+# Conditions if: the result is iqual or less than 24.99
+elif BMI <= 24.99:
+        print("A person with a BMI of " + str((round(BMI,2))) + " is normal weight ")
+
+# Conditions if: the result is high than 24.9999 print this statment
+else:
+        print("A person with a BMI of " + str((round(BMI,2))) + " is overweight ")
+
+input('Please press Enter to exit')
 
 
 
