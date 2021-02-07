3e3e79b14a55005c62885ad9c0880ab8b9309c8a
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-07 18:31:31 +0000

    lecture

---
+++ week03/firstVariables.py
@@ -0,0 +1,34 @@
+TxF = True
+print (type(TxF))
+
+Name = 'Ricardo'
+print (type(Name))
+
+age = 41
+print (type(age))
+
+age = ()
+print (type(age))
+
+age = {}
+Age = 3
+print (type(age))
+print (type(Age))
+
+ageOfPatient = {}
+age = 3
+print (type(ageOfPatient))
+print (type(age))
+
+print ('you are ' + str(age))
+
+
+ageOfPatient = {}
+age = '3'
+print ('type of age of patient is: ' + str (type(ageOfPatient)))
+print ('type of age is: ' + str(type(age)))
+
+
+print ('you are ' + str(age))
+nextYear = age + 1
+print ('next year you will be ' + nextYear)
