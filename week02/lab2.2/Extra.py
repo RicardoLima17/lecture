2131a89c463389335a7c326fb1e4e781fdb7024a
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-09 22:59:46 +0000

    week02

---
+++ week02/lab2.2/Extra.py
@@ -0,0 +1,25 @@
+
+# Author Ricardo
+
+# 17
+name = input("Enter your name ")
+Age =  input("Enter your age ")
+print('Hello {} your age is {}'.format(name, Age))
+
+# 18
+name = input("Enter your name ")
+Age =  input("Enter your age ")
+print('Hello {} \tyour age is {}'.format(name, Age))
+
+name = input("Enter your name ")
+Age =  input("Enter your age ")
+print('Hello {} \nyour age is {}'.format(name, Age))
+
+# 19
+number = int(input("enter a number "))
+number1 = int(input("enter the second a number "))
+result = number-number1
+print ("{}".format(result))
+
+# 20
+print (2==3)
