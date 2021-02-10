6808ea091ef55d5b032b044cd9853c51c8c50389
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-10 20:50:32 +0000

    week03

--- week03/weekly task 03/secondstring.py.py
+++ week03/weekly task 03/secondstring.py.py
@@ -1,6 +1,11 @@
-string = 'The quick brown fox jumps over the lazy dog.' [::1]
 
-print (string)
 
+# Author Ricardo
+
+# type  The quick brown fox jumps over the lazy dog.
+sentence = input("Type a sentence that you want reversed:")
+
+# reverse a sentence every second letter in reverse order.
+print(sentence[::-2])
 
 
