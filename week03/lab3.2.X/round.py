1df22cb27ab4591be2663e2b101098394eeb374b
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-10 19:50:34 +0000

    week03

---
+++ week03/lab3.2.X/round.py
@@ -0,0 +1,10 @@
+
+# Author: Ricardo
+
+# floot for decimal number, type your number
+numberToRound = float(input("Enter a float number:"))
+
+# round the number
+roundedNumber = round(numberToRound)
+print ( '{} rounded is {}'.format(abs(numberToRound),roundedNumber))
+
