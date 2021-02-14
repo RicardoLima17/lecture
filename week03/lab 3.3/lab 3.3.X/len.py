67609625caf4fd43c48dc2ccdd770e8efcc86598
Ricardo Oliveira Lima lima <limaricardooliveira@gmail.com>
2021-02-14 22:15:25 +0000

    week03

---
+++ week03/lab 3.3/lab 3.3.X/len.py
@@ -0,0 +1,11 @@
+
+# Author Ricardo
+
+inputString = input('enter a string:')
+
+# len count all caracter including space
+lenghtOfString = len(inputString)
+print('The lenght of {} is {}'.format(inputString, lenghtOfString))
+
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print (message)
